6fffc324f528e171d653388d8c6cccabc9358c0c
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-02-13 12:19:40 +0530

    semi final part of the day done

--- src/main/java/controller/SignUpServlet.java
+++ src/main/java/controller/SignUpServlet.java
@@ -1,6 +1,49 @@
 package controller;
 
-public class SignUpServlet {
+import java.io.IOException;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dto.TodoUser;
+
+@WebServlet("/signup")
+public class SignUpServlet extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name=req.getParameter("name");
+		String email=req.getParameter("email");
+		String password=req.getParameter("password");
+		Long mobile=Long.parseLong(req.getParameter("mobile"));
+		String dob=req.getParameter("dob");
+		String gender=req.getParameter("gender");
+		
+		EntityManagerFactory factory=Persistence.createEntityManagerFactory("abc");
+		EntityManager manager=factory.createEntityManager();
+		EntityTransaction transaction=manager.getTransaction();
+		
+		TodoUser user=new TodoUser();
+		user.setName(name);
+		user.setEmail(email);
+		user.setPassword(password);
+		user.setMobile(mobile);
+		user.setDob(dob);
+		user.setGender(gender);
+		
+		transaction.begin();
+		manager.persist(user);
+		transaction.commit();
+		
+		resp.getWriter().print("<h1 align='center' style='color:green'>Account Created  Successfully</h1>");
+		req.getRequestDispatcher("login.html").include(req, resp);
+	}
 	
 }
--- src/main/java/dto/TodoUser.java
+++ src/main/java/dto/TodoUser.java
@@ -1,5 +1,25 @@
 package dto;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
 public class TodoUser {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	String name;
+	String email;
+	String password;
+	Long mobile;
+	String dob;
+	String gender;
+	
+	
 }
